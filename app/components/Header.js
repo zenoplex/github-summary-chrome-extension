826da76e8c9f313750dad5802b655d7cfd3b11c1
Gen Ichino <zenoplex@gmail.com>
2016-05-25 21:48:37 +0900

    feat: update Header component

--- app/components/Header.js
+++ app/components/Header.js
@@ -2,11 +2,12 @@ import React, { PropTypes } from 'react';
 import AppBar from 'material-ui/AppBar';
 import IconButton from 'material-ui/IconButton';
 import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import Extension from 'material-ui/svg-icons/action/extension';
 
 const Header = ({ onIconRightTap }) =>
   <AppBar
-    title="Title"
-    showMenuIconButton={false}
+    title="GitHub summary extension"
+    iconElementLeft={<IconButton><Extension /></IconButton>}
     iconElementRight={
       <IconButton onTouchTap={onIconRightTap}><MoreVertIcon /></IconButton>
     }
