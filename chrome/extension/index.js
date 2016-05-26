1aefa7178af297446e40c77acf66d9daee9f2770
Gen Ichino <zenoplex@gmail.com>
2016-05-26 21:30:11 +0900

    refactor: exec runSaga

--- chrome/extension/index.js
+++ chrome/extension/index.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from '../../app/containers/Root';
+import configureStore from '../../app/store/configureStore';
+import sagas from '../../app/sagas';
 import './style.css';
 
 chrome.storage.local.get('state', ({ state }) => {
   const initialState = JSON.parse(state || '{}');
+  const store = configureStore(initialState);
+  store.runSaga(sagas);
 
-  const createStore = require('../../app/store/configureStore');
   ReactDOM.render(
-    <Root store={createStore(initialState)} />,
+    <Root store={store} />,
     document.querySelector('#root')
   );
 });
