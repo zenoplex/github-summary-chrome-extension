85beaf35f119dde864af73ae7c12054522f9c581
Gen Ichino <zenoplex@gmail.com>
2016-05-26 14:46:39 +0900

    feat: add sidebar related action types

--- app/constants/ActionTypes.js
+++ app/constants/ActionTypes.js
@@ -1,10 +1,5 @@
-export const ADD_TODO = 'ADD_TODO';
-export const DELETE_TODO = 'DELETE_TODO';
-export const EDIT_TODO = 'EDIT_TODO';
-export const COMPLETE_TODO = 'COMPLETE_TODO';
-export const COMPLETE_ALL = 'COMPLETE_ALL';
-export const CLEAR_COMPLETED = 'CLEAR_COMPLETED';
+export const OPEN_SIDEBAR = 'OPEN_SIDEBAR';
+export const CLOSE_SIDEBAR = 'CLOSE_SIDEBAR';
+export const TOGGLE_SIDEBAR = 'TOGGLE_SIDEBAR';
 
-export const OPEN_DRAWER = 'OPEN_DRAWER';
-export const CLOSE_DRAWER = 'CLOSE_DRAWER';
-export const TOGGLE_DRAWER = 'TOGGLE_DRAWER';
+export const SAVE_SETTINGS = 'SAVE_SETTINGS';
