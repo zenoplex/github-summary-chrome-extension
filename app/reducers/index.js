27d95645d3322b6114043040999471976fc10225
Gen Ichino <zenoplex@gmail.com>
2016-05-31 20:57:23 +0900

    feat: combine summary and form reducer to root reducer

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,8 +1,12 @@
 import { combineReducers } from 'redux';
 import settings from './settings';
 import sidebar from './sidebar';
+import summary from './summary';
+import { reducer as form } from 'redux-form';
 
 export default combineReducers({
   settings,
   sidebar,
+  summary,
+  form,
 });
