174b4e802164d5ffdcc536513e39946e31d77b4e
Gen Ichino <zenoplex@gmail.com>
2016-05-31 21:17:59 +0900

    feat: add generateSummary saga

--- app/sagas/index.js
+++ app/sagas/index.js
@@ -1,17 +1,23 @@
 import { take, put, call, fork, select } from 'redux-saga/effects';
+import GithubSummary from 'github-summary';
 import * as types from '../constants/ActionTypes';
 
-console.log(put, call, fork, select);
-
-function* fetchSummary() {
+function* generateSummary() {
   while (true) {
-    yield take(types.TOGGLE_SIDEBAR);
+    yield take(types.GENERATE_SUMMARY);
+
+    const { username, token } = yield select(state => state.settings);
+    const githubSummary = new GithubSummary({
+      username,
+      token,
+    });
+    const summary = yield call(githubSummary.getSummary.bind(githubSummary));
+    yield put({ type: types.RECEIVE_SUMMARY, payload: summary });
   }
 }
 
-
 export default function* root() {
   yield [
-    fork(fetchSummary),
+    fork(generateSummary),
   ];
 }
