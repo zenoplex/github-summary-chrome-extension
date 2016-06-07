a74a173cd28e194044d8dc2b76ca1bdf2f54c2e9
Gen Ichino <zenoplex@gmail.com>
2016-06-07 19:41:51 +0900

    refactor: rewrite saga

--- app/sagas/index.js
+++ app/sagas/index.js
@@ -1,20 +1,26 @@
-import { take, put, call, fork, select } from 'redux-saga/effects';
+import { takeLatest } from 'redux-saga';
+import { put, call, fork, select } from 'redux-saga/effects';
 import GithubSummary from 'github-summary';
 import * as types from '../constants/ActionTypes';
 
-function* generateSummary() {
-  while (true) {
-    yield take(types.GENERATE_SUMMARY);
-
+function* fetchSummary() {
+  try {
     const { username, token } = yield select(state => state.settings);
     const githubSummary = new GithubSummary({
       username,
       token,
     });
-    const summary = yield call(githubSummary.getSummary.bind(githubSummary));
-    yield put({ type: types.RECEIVE_SUMMARY, payload: summary });
+    const summary = yield call([githubSummary, githubSummary.getSummary]);
+    yield put({ type: types.SUMMARY_FETCH_SUCCESS, payload: summary });
+  } catch (err) {
+    yield put({ type: types.SUMMARY_FETCH_FAIL });
   }
 }
+
+function* generateSummary() {
+  yield* takeLatest(types.SUMMARY_FETCH_REQUEST, fetchSummary);
+}
+
 
 export default function* root() {
   yield [
