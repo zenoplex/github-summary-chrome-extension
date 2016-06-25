ca1322b604123d6fe23d2a5b74ba6896cbc05f4d
Gen <zenoplex@gmail.com>
2016-06-25 22:41:46 +0900

    chore: add declares for external lib

---
+++ app/declares/material-ui.js
@@ -0,0 +1,7 @@
+declare module 'material-ui' {
+  declare var exports: any;
+}
+
+declare module 'material-ui/Card' {
+  declare var exports: any;
+}
---
+++ app/declares/redux-form.js
@@ -0,0 +1,3 @@
+declare module 'redux-form' {
+  declare var exports: any;
+}
